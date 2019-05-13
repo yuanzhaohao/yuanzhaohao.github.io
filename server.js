5256f2b0ac32b4d2146abb3ea67b4e8d6d43216f
yuanzhaohao <yuanzhaohao@163.com>
2019-05-13 20:22:47 +0800

    ! feat: update firework

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 var mine = {
-  'css': 'text/css',
-  'gif': 'image/gif',
-  'html': 'text/html',
-  'ico': 'image/x-icon',
-  'jpeg': 'image/jpeg',
-  'jpg': 'image/jpeg',
-  'js': 'text/javascript',
-  'json': 'application/json',
-  'pdf': 'application/pdf',
-  'png': 'image/png',
-  'svg': 'image/svg+xml',
-  'swf': 'application/x-shockwave-flash',
-  'tiff': 'image/tiff',
-  'txt': 'text/plain',
-  'wav': 'audio/x-wav',
-  'wma': 'audio/x-ms-wma',
-  'wmv': 'video/x-ms-wmv',
-  'xml': 'text/xml'
+  css: 'text/css',
+  gif: 'image/gif',
+  html: 'text/html',
+  ico: 'image/x-icon',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpeg',
+  js: 'text/javascript',
+  json: 'application/json',
+  pdf: 'application/pdf',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  swf: 'application/x-shockwave-flash',
+  tiff: 'image/tiff',
+  txt: 'text/plain',
+  wav: 'audio/x-wav',
+  wma: 'audio/x-ms-wma',
+  wmv: 'video/x-ms-wmv',
+  xml: 'text/xml',
 };
 
 var server = http.createServer(function(request, response) {
@@ -34,13 +34,14 @@ var server = http.createServer(function(request, response) {
   if (ext) {
     ext = ext.slice(1);
   } else {
-    realPath += 'index.html';
+    realPath += realPath.charAt(realPath.length - 1) === '/' ? 'index.html' : '/index.html';
+    ext = 'html';
   }
 
   fs.exists(realPath, function(isExist) {
     if (!isExist) {
       response.writeHead(404, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/plain',
       });
       response.write('This request URL ' + pathname + ' was not found on this server.');
       response.end();
@@ -48,14 +49,13 @@ var server = http.createServer(function(request, response) {
       fs.readFile(realPath, 'binary', function(err, file) {
         if (err) {
           response.writeHead(500, {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
           });
           response.end(err);
         } else {
           var contentType = mine[ext] || 'text/plain';
-          console.log(contentType);
           response.writeHead(200, {
-            'Content-Type': contentType
+            'Content-Type': contentType,
           });
           response.write(file, 'binary');
           response.end();
@@ -63,7 +63,6 @@ var server = http.createServer(function(request, response) {
       });
     }
   });
-
 });
 
 server.listen(PORT);
