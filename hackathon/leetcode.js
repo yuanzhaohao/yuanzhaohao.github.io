3705c965e62e4d6dce7c5a297190d3b8ce700e37
yuanzhaohao <yuanzhaohao@163.com>
2019-05-26 15:55:50 +0800

    ! feat: add leetcode

---
+++ hackathon/leetcode.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * valid-parentheses/
+ */
+
+var isValid = function(s) {
+  if (s === '') {
+    return true;
+  }
+  const stack = [];
+  const left = ['(', '[', '{'];
+  const right = [')', ']', '}'];
+  for (let i = 0; i < s.length; i++) {
+    const item = s[i];
+    if (left.indexOf(item) !== -1) {
+      stack.push(item);
+      continue;
+    }
+    if (right.indexOf(item) !== -1) {
+      const p = stack.pop();
+      switch (item) {
+        case ')':
+          if (p !== '(') {
+            return false;
+          }
+          break;
+        case '}':
+          if (p !== '{') {
+            return false;
+          }
+          break;
+        case ']':
+          if (p !== '[') {
+            return false;
+          }
+          break;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid(''));
+console.log(isValid('[['));
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('([)]'));
+console.log(isValid('{[]}'));
