ce0517ec0a75fe76d583944342abb2b8353c9f29
yuanzhaohao <yuanzhaohao@163.com>
2019-08-05 15:37:59 +0800

    ! feat: prepare

--- hackathon/prepare.js
+++ hackathon/prepare.js
@@ -414,3 +414,23 @@ function throttleHard(fn, ms = 150, context) {
 }
 
 // setInterval(throttleHard(fnT, 1000), 50);
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right) {
+    return sum === root.val;
+  }
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+};
