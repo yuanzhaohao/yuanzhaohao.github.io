6607d5325162afbe94ecd63c329b6e6f7c9c7479
yuanzhaohao <yuanzhaohao@163.com>
2019-09-21 19:58:13 +0800

    ! feat: update prepare

--- hackathon/prepare0919.js
+++ hackathon/prepare0919.js
@@ -1,3 +1,5 @@
+// 深拷贝
+//
 function clone(originalSource) {
   const cloneObject = {};
   const stack = [
@@ -12,7 +14,6 @@ function clone(originalSource) {
 
   while ((current = stack.shift())) {
     const keys = Object.keys(current.source);
-    console.log(keys);
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
       let value = current.source[key];
@@ -96,3 +97,91 @@ let obj = {
 
 console.log(clone(obj));
 console.log(cloneDeep(obj));
+
+// 非严格比对
+//
+if ([] == false) {
+  console.log(1);
+}
+if ({} == false) {
+  console.log(2);
+}
+if ([]) {
+  console.log(3);
+}
+if ([1] == [1]) {
+  console.log(4);
+}
+
+/**
+ * script start
+ * a1 start
+ * a2
+ * promise2
+ * script end
+ * promise1
+ * a1 end
+ * promise2.then
+ * promise3
+ * setTimeout
+ */
+async function a1() {
+  console.log('a1 start');
+  await a2();
+  console.log('a1 end');
+}
+async function a2() {
+  console.log('a2');
+}
+
+console.log('script start');
+
+setTimeout(() => {
+  console.log('setTimeout');
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log('promise1');
+});
+
+a1();
+
+let promise2 = new Promise(resolve => {
+  resolve('promise2.then');
+  console.log('promise2');
+});
+
+promise2.then(res => {
+  console.log(res);
+  Promise.resolve().then(() => {
+    console.log('promise3');
+  });
+});
+console.log('script end');
+
+/**
+ * bind的实现
+ */
+Function.prototype.myBind = function(context) {
+  const args = Array.prototype.slice.call(arguments, 1);
+  const self = this;
+
+  return function() {
+    self.apply(context, args.concat(Array.prototype.slice.call(arguments)));
+  };
+};
+
+/**
+ * 函数节流
+ */
+function throttle(fn, ms = 150, context) {
+  let lastTime;
+
+  return function() {
+    let newTime = !new Date();
+    const args = arguments;
+    if (!lastTime || newTime - lastTime >= ms) {
+      fn.apply(context, args);
+    }
+  };
+}
